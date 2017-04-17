b71e194702919114a0988f510e361115a0821d39
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-04-17 16:18:47 -0300

    LocalStorage && Deletar a lista

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-
-var list = [
-    {"desc":"rice","amount":"1","value":"5.40"},
-    {"desc":"beer","amount":"12","value":"1.99"},
-    {"desc":"meat","amount":"1","value":"15.00"}
-];
-
 function getTotal(list){
     var total = 0;
     for(var key in list){
         total += list[key].value * list[key].amount;
     }
-    return total;
+    document.getElementById("total-value").innerHTML = formatValue(total);
 }
 
 function setList(list){
+	getTotal(list);
+	saveListStorage(list);
 	var table = '';
 	table += '<thead>';
 	table += '<tr>';
@@ -25,19 +20,19 @@ function setList(list){
 	table += '</thead>';
 	table += '<tbody>';
 
-	list.forEach(function(element, index, array){
+	for(var key in list){
 		table += '<tr>';
-		table += '<td>' + formatDesc(element.desc) + '</td>';
-		table += '<td>' + element.amount + '</td>';
-		table += '<td>' + formatValue(element.value) + '</td>';
+		table += '<td>' + formatDesc(list[key].desc) + '</td>';
+		table += '<td>' + formatAmount(list[key].amount) + '</td>';
+		table += '<td>' + formatValue(list[key].value) + '</td>';
 		table += '<td>';
 		table += '<div class="btn-group">';
-		table += '<button class="btn btn-default" onclick="setUpdate(' + index + ');">Edit</button>';
-		table += '<button class="btn btn-default" onclick="deleteData(' + index + ');">Delete</button>';
+		table += '<button class="btn btn-default" onclick="setUpdate(' + key + ');">Edit</button>';
+		table += '<button class="btn btn-default" onclick="deleteData(' + key + ');">Delete</button>';
 		table += '</div>';
 		table += '</td>';
 		table += '</tr>';
-	});
+	}
 
 	table += '</tbody>';
     document.getElementById("list-table").innerHTML = table;
@@ -49,6 +44,10 @@ function formatDesc(desc){
 	return str;
 }
 
+function formatAmount(value){
+	return parseInt(value);
+}
+
 function formatValue(value){
 	var str = parseFloat(value).toFixed(2) + "";
 	str = str.replace(".", ",");
@@ -57,6 +56,9 @@ function formatValue(value){
 }
 
 function addData(){
+	if(!validation()){
+		return;
+	}
 	var desc 	= document.getElementById('desc').value;
 	var amount 	= document.getElementById('amount').value;
 	var value 	= document.getElementById('value').value;
@@ -78,7 +80,7 @@ function setUpdate(id){
 	document.getElementById("value").value 	= obj.value;
 
 	changeState("editing");
-	document.getElementById("inputIdUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="' + id + '">';
+	document.getElementById("input-id-update").innerHTML = '<input id="idUpdate" type="hidden" value="' + id + '">';
 }
 
 function resetForm(){
@@ -87,7 +89,7 @@ function resetForm(){
 	document.getElementById("value").value 	= "";
 
 	changeState("default");
-	document.getElementById("inputIdUpdate").innerHTML = '';
+	document.getElementById("input-id-update").innerHTML = '';
 }
 
 function changeState(state){
@@ -105,6 +107,9 @@ function changeState(state){
 }
 
 function updateData(){
+	if(!validation()){
+		return;
+	}
 	var id 		= document.getElementById("idUpdate").value;
 	var desc 	= document.getElementById('desc').value;
 	var amount 	= document.getElementById('amount').value;
@@ -133,4 +138,72 @@ function deleteData(id){
 	}
 }
 
-setList(list);
+function validation(){
+
+	document.getElementById("errors").style.display = 'none';
+
+	var desc 	= document.getElementById("desc").value;
+	var amount 	= document.getElementById("amount").value;
+	var value 	= document.getElementById("value").value;
+	var errors 	= "";
+
+	if (desc === "") {
+		errors += '<p>Fill out description</p>';
+	}
+
+	if (amount === "") 
+	{
+		errors += '<p>Fill out amount</p>';
+	}
+	else if(amount != parseInt(amount))
+	{
+		errors += '<p>Fill out with a valid number</p>';
+	}
+
+	if (value === "") 
+	{
+		errors += '<p>Fill out value</p>';
+	}
+	else if(value != parseFloat(value))
+	{
+		errors += '<p>Fill out with a valid number</p>';
+	}
+
+	if (errors != "") {
+		document.getElementById("errors").style.display = 			'block';
+		document.getElementById("errors").style.backgroundColor = 	'rgba(85, 85, 85, 0.3)';
+		document.getElementById("errors").style.color = 			'white';
+		document.getElementById("errors").style.padding = 			'10px';
+		document.getElementById("errors").style.margin = 			'10px';
+		document.getElementById("errors").style.borderRadius = 		'13px';
+		document.getElementById("errors").innerHTML = 				'<h3 style="color: red">Error:</h3>' + errors;
+		return 0;
+	}else{
+		return 1;
+	}
+
+}
+
+function deleteList(){
+	if (confirm("Do you want to delete this list?")) {
+		list = [];
+		setList(list);
+	}
+}
+
+function saveListStorage(list){
+	var jsonStr = JSON.stringify(list);
+	localStorage.setItem('list', jsonStr);
+}
+
+function initListStorage(){
+	var testList = localStorage.getItem('list');
+	if (testList) {
+		list = JSON.parse(testList);
+	}else{
+		list = JSON.parse([]);
+	}
+	setList(list);	
+}
+
+initListStorage();
